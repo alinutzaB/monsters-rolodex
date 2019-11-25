72b228435c19d65f6e470263a4c9c3bf751a3da8
alinutzaB <alinutza.ms@gmail.com>
2019-11-25 13:25:15 +0200

    Updates

---
+++ precache-manifest.4826f71f5b6fb71fa386db84823ae66a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f82168381e171435ddbe3597ff2978",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f07a92a2b52cc0226fea",
+    "url": "/monsters-rolodex/static/css/main.85e6316f.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "f07a92a2b52cc0226fea",
+    "url": "/monsters-rolodex/static/js/main.1b13cedb.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
